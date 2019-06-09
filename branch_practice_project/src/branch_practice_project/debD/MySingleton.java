c74ebbc6c12b7afe98990b5bf0f2461d22b755bd
Debajyoti17 <debu.igit13@gmail.com>
2019-06-10 04:09:14 +0530

    Singleton demo

---
+++ branch_practice_project/src/branch_practice_project/debD/MySingleton.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package branch_practice_project.debD;
+
+/**
+ * Desc : 
+ * @author Debu-PC_2
+ * Date : 24-Sep-2018 10:28:03 PM
+ */
+public class MySingleton {
+
+	/**
+	 * Desc : 
+	 * @author Debu-PC_2
+	 * Date : 24-Sep-2018 10:28:03 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
